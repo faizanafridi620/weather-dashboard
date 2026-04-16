2176b936e881b534b641e63f7bdfc5d26f581cbb
faizanafridi620 <faizanafridi676@gmail.com>
2026-04-16 23:07:14 +0530

    add zoomin functionality

--- src/components/WeatherChart.jsx
+++ src/components/WeatherChart.jsx
@@ -1,40 +1,104 @@
 import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
+  Chart as ChartJS,
+  LineElement,
+  PointElement,
+  LinearScale,
+  CategoryScale,
   Tooltip,
-  ResponsiveContainer,
-  Brush,
-  BarChart,
-  Bar,
-} from "recharts";
+  Legend,
+  BarElement,
+} from "chart.js"
+import zoomPlugin from "chartjs-plugin-zoom"
+import {Line, Bar} from "react-chartjs-2"
+import { useRef } from "react";
+
+ChartJS.register(
+  LineElement,
+  BarElement,
+  PointElement,
+  LinearScale,
+  CategoryScale,
+  Tooltip,
+  Legend,
+  zoomPlugin
+)
 
 function WeatherChart({ title, data, chartType = "line" }) {
+  const chartRef = useRef();
+  const isMultiLine = data && data[0]?.pm10 !== undefined;
+
+  const chartData = {
+    labels: data.map((d) => d.time),
+    datasets: isMultiLine ? [
+      {
+        label: "PM10",
+        data: data.map((d) => d.pm10),
+        borderWidth: 2,
+        borderColor: "#ff7300",
+      },
+      {
+        label: "PM2.5",
+        data: data.map((d) => d.pm2_5),
+        borderWidth: 2,
+        borderColor: "#387908",
+      }
+    ] : [
+      {
+        label: title,
+        data: data.map((d) => d.value),
+        borderWidth: 2,
+        borderColor: "#8884d8",
+        backgroundColor: "#6366f1"
+      }
+    ]
+  }
+
+  const options ={
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+      },
+      zoom: {
+        pan: {
+          enabled: true,
+          mode: "x"
+        },
+        zoom: {
+          wheel: {
+            enabled: true,
+          },
+          pinch: {
+            enabled: true,
+          },
+          mode: "x",
+        }
+      }
+    },
+    scales: {
+      x: {
+        ticks: {
+          maxTicksLimit: 10,
+        }
+      }
+    }
+  }
   return (
     <div className="bg-white/80 backdrop-blur-md p-5 rounded-2xl shadow-lg">
       <h2 className="mb-3 font-semibold text-lg text-gray-700">{title}</h2>
-      <ResponsiveContainer width="100%" height={300}>
+      <button 
+      className="px-3 py-1 text-sm bg-blue-500 text-white rounded"
+      onClick={() => chartRef.current.resetZoom()}
+      >
+        Reset
+      </button>
         {chartType === "line" ? (
-          <LineChart data={data}>
-            <XAxis dataKey="time" hide />
-            <YAxis />
-            <Tooltip />
-            <Line type="monotone" dataKey="value" stroke="#8884d8" />
-            <Brush dataKey="time" height={20} />
-            <Line type="monotone" dataKey="pm10" stroke="#ff7300" />
-            <Line type="monotone" dataKey="pm2_5" stroke="#387908" />
-          </LineChart>
+
+          <Line ref={chartRef} data={chartData} options={options} />
         ) : (
-          <BarChart data={data}>
-            <XAxis dataKey="time" hide />
-            <YAxis />
-            <Tooltip />
-            <Bar dataKey="value" fill="#ff7300" />
-            <Brush dataKey="time" height={20} />
-          </BarChart>
+
+          <Bar ref={chartRef} data={chartData} options={options} />
         )}
-      </ResponsiveContainer>
     </div>
   );
 }
